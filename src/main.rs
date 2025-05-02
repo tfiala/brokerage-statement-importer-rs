a6e2ae76e2c56ee5e56c2d95e952bb9985a5519e
Todd Fiala <todd.fiala@gmail.com>
2025-05-02 11:37:36 -0700

    wip: adding command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(name = "tdb")]
+#[command(version, about = "trader database cli", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Import brokerage statements into the db
+    #[command(arg_required_else_help = true)]
+    Import {
+        /// Statement filenames to import.
+        #[arg(required = true)]
+        path: Vec<PathBuf>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Import { path } => {
+            println!("importing paths: {:?}", path);
+        }
+    }
 }
